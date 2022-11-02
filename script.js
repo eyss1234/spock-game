5237a4d59f9dcdf5cda499f8228d5b474f472d69
Emily Santos <eyssantos@gmail.com>
2022-11-02 15:19:54 +0000

    reset functionality

--- script.js
+++ script.js
@@ -37,6 +37,18 @@ function resetSelected() {
   });
 }
 
+// Reset Score and playerChoice/computerChoice
+function resetAll() {
+  playerScoreNumber = 0;
+  computerScoreNumber = 0;
+  playerScoreEl.textContent = playerScoreNumber;
+  computerScoreEl.textContent = computerScoreNumber;
+  playerChoiceEl.textContent = '';
+  computerChoiceEl.textContent = '';
+  resultText.textContent = '';
+  resetSelected();
+}
+
 // Random computer choice
 function computerRandomChoice() {
   const computerChoiceNumber = Math.random();
@@ -138,3 +150,6 @@ function select(playerChoice) {
       break;
   }
 }
+
+// On startup, set initial values
+resetAll();
